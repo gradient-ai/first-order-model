b09e64dcfead0bf5667d58f3b1e468e251cba983
Kash Money <knouroozi@gmail.com>
2021-09-21 17:09:49 -0400

    Add audio scaffolding

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 # Model Setup
 import io
 import os
+import subprocess
 
 from base64 import b64decode
 from os import environ as env
@@ -13,12 +14,18 @@ from skimage import img_as_ubyte
 
 from demo import load_checkpoints, make_animation
 
-gen, kp = load_checkpoints("config/vox-256.yaml", env["CHECKPOINT_PATH"])
+gen, kp = load_checkpoints("config/vox-256.yaml", env["MODEL"])
 
-_video_reader = imageio.get_reader(env["DRIVING_VIDEO_PATH"])
+_video_reader = imageio.get_reader(env["DRIVING_VIDEO"])
 
 fps = _video_reader.get_meta_data()["fps"]
-driving_video = [resize(frame, (256, 256))[..., :3] for frame in _video_reader]
+driving_video = []
+try:
+    for im in _video_reader:
+        driving_video.append(resize(im, (256, 256))[..., :3])
+except RuntimeError:
+    pass
+_video_reader.close()
 
 
 def generate_adoro(from_image: str, base_path: str):
@@ -31,7 +38,20 @@ def generate_adoro(from_image: str, base_path: str):
         frame_bytes = img_as_ubyte(frame)
         imageio.imwrite(f"{base_path}/{index}.jpg", frame_bytes)
         frames.append(frame_bytes)
-    imageio.mimsave(f"{base_path}.mp4", frames, fps=fps)
+
+    generated_video_path = f"{base_path}/generated.mp4"
+    imageio.mimsave(generated_video_path, frames, fps=fps)
+
+    video_in = f"""-an -i '{generated_video_path}'"""
+    audio_in = f"""-vn -i '{audio_path}'"""
+    watermark_in = f"""-an -i '{watermark_path}'"""
+    processing = f"""-map 0:v -map 1:a -filter_complex '[0:v][2:v]overlay=156:12'"""
+    video_out = f"'{base_path}.mp4'"
+    subprocess.run(
+        f"""
+        ffmpeg {video_in} {audio_in} {watermark_in} {processing} -y -shortest {video_out}
+        """,
+        check=True, shell=True, timeout=60)
 
 # API Server
 from fastapi import FastAPI, BackgroundTasks
@@ -41,9 +61,10 @@ from pydantic import BaseModel
 
 app = FastAPI()
 app.mount("/public", StaticFiles(directory="public"), name="static_files")
+app.mount("/", StaticFiles(directory="web", html=True), name="web")
 
 origins = [
-    "http://localhost:3000",
+    "*",
 ]
 
 app.add_middleware(
